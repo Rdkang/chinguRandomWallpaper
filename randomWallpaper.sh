fb55ba57d1fe57f89cd3731744c6143a836d8a27
rdkang <rdkang@arch.com>
2022-05-20 12:30:26 +0100

    log the previous wallpapers to a file

--- randomWallpaper.sh
+++ randomWallpaper.sh
@@ -12,6 +12,7 @@ function main() {
   width=$(identify -format "%w" $choice)
   height=$(identify -format "%h" $choice)
 
+
   # if resolution of picture is agove 1080p, then will set wallpaper
     # otherwise will call main funtion again and try with another choice
   if [ $width -ge $minWidth ] && [ $height -ge $minHeight ]
@@ -20,7 +21,8 @@ function main() {
     xwallpaper --zoom $choice
 
     echo -e "Wallpaper is\n $choice"
-    notify-send "Wallpaper" "$choice"
+    echo -e "$choice" >> $scriptDirectory/wallpaperLog.log
+    #notify-send "Wallpaper" "$choice"
     exit 0
   else 
     # call function again to find a new compatible picture
